15dc1a49882fcc9c8c036e14e936a309fe1e6041
Venedykt <chewbaccayeti@gmail.com>
2023-04-17 15:57:53 +0300

    Bug fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-// import { Component } from 'react';
-import { GlobalStyle } from 'GlobalStyle';
 import { FeedbackOptions } from './Feedback/FeedbackOptions/FeedbackOptions';
 import { Notifications } from './Feedback/Notifications/Notifications';
 import { Section } from './Feedback/Section/Section';
 import { Statistics } from './Feedback/Statistics/Statistics';
+import { GlobalStyle } from 'GlobalStyle';
 import { Container } from './Layout/Layout.styled';
 
 export const App = () => {
@@ -22,8 +21,11 @@ export const App = () => {
         return setNeutral(s => s + 1);
       case 'bad':
         return setBad(s => s + 1);
+      default:
+        return;
     }
   };
+
   const countTotalFeedback = () =>
     Object.values(feedback).reduce((total, prevState) => total + prevState, 0);
 
@@ -45,8 +47,8 @@ export const App = () => {
             good={good}
             neutral={neutral}
             bad={bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
         ) : (
           <Notifications message="There is no feedback" />
@@ -57,62 +59,3 @@ export const App = () => {
     </Container>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   handleBtnClick = event => {
-//     const value = event.target.value;
-
-//     this.setState(prevState => {
-//       return { [value]: prevState[value] + 1 };
-//     });
-//   };
-
-//   countTotalFeedback = () =>
-//     Object.values(this.state).reduce(
-//       (total, prevState) => total + prevState,
-//       0
-//     );
-
-//   countPositiveFeedbackPercentage = () =>
-//     Math.round(
-//       (this.state.good /
-//         Object.values(this.state).reduce(
-//           (total, feedback) => total + feedback,
-//           0
-//         )) *
-//         100
-//     );
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     return (
-//       <Container>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys({ good, neutral, bad })}
-//             onLeaveFeedback={this.handleBtnClick}
-//           ></FeedbackOptions>
-//         </Section>
-//         <Section title="Statistics">
-//           {this.countTotalFeedback() > 0 ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback}
-//               positivePercentage={this.countPositiveFeedbackPercentage}
-//             />
-//           ) : (
-//             <Notifications message="There is no feedback" />
-//           )}
-//         </Section>
-//         <GlobalStyle />
-//       </Container>
-//     );
-//   }
-// }
